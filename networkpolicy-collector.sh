75ca15fbd2917646f06467d352f9a19fcb78536d
Domenic Mayer <domenic@dropdown.ch>
2019-04-01 22:09:27 +0200

    Added some lines in networkpolicy-collector.sh

--- networkpolicy-collector.sh
+++ networkpolicy-collector.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+echo "Start infinite loop to collect network policies."
+
 while true
-do
-  echo "Hello kuberecon world."
+do 
+  i="0"
+  LENGTH="0"
+
+  /usr/local/bin/kubectl get networkpolicies --all-namespaces -o json > /home/collector/all-networkpolicies.json
+  LENGTH=$(jq '.items | length' /home/collector/all-networkpolicies.json)
+
+  mkdir -p /home/collector/single-networkpolicies
+
+  while [ $i -lt $LENGTH ]
+    do
+      jq ".items[$i]" /home/collector/all-networkpolicies.json > /home/collector/single-networkpolicies/np$i.json
+      i=$[$i+1]
+    done
+  rm -rf /home/collector/single-networkpolicies
   sleep 5
 done
